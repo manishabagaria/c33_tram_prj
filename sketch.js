cf5ff2b91180dc14f37f9e0d57131e59e9d3e486
manisha bagaria <manishabagaria@gmail.com>
2021-04-10 16:11:33 +0530

    add

---
+++ sketch.js
@@ -0,0 +1,37 @@
+const Engine= Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var myEngine, myWorld;
+var bg;
+var ground;
+var boggie1,boggie2,boggie3;
+var chain1;
+var trainSound 
+var crashSound
+var flag = 0;
+
+function preload(){
+  bg= loadImage("images/bg.jpg");
+  trainSound = loadSound("sound/train.mp3");
+  crashSound = loadSound("sound/train_crossing.mp3");
+}
+function setup() {
+  createCanvas(1200,400);
+  myEngine = Engine.create();
+  myWorld= myEngine.world;
+
+  ground = new Ground(600,380,1200,20);
+
+
+}
+
+function draw() {
+  background(bg);  
+  Engine.update(myEngine);
+
+ 
+  }
+
+  
